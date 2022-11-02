e9a0fd5553961cd5de8d1e5afd8db786eb9886a7
PricedOx <deadly.devils@outlook.com>
2022-11-02 20:37:54 +0000

    actually added the stuff

---
+++ discord-RPC.py
@@ -0,0 +1,53 @@
+import time
+#importing the variables
+from editables import APPLICATION_ID, activity_state, activity_party_size, activity_party_max_size, activity_secrets_join, activity_party_id, activity_large_image, activity_large_text, activity_small_image, activity_small_text
+
+import discordsdk as sdk
+
+
+
+app = sdk.Discord(APPLICATION_ID, sdk.CreateFlags.default)
+
+activity_manager = app.get_activity_manager()
+
+#set variables for discord rich presence
+activity = sdk.Activity()
+#set variables for discord rich presence
+#if a variable is blank, it will not be displayed
+if activity_state != "":
+    activity.state = activity_state
+
+
+if activity_party_size != "":
+    activity.party.size.current_size = activity_party_size
+if activity_party_max_size != "":
+    activity.party.size.max_size = activity_party_max_size
+if activity_secrets_join != "":
+    activity.secrets.join = activity_secrets_join
+if activity_party_id != "":
+    activity.party.id = activity_party_id
+
+
+if activity_large_image != "":
+    activity.assets.large_image = activity_large_image
+if activity_large_text != "":
+    activity.assets.large_text = activity_large_text
+if activity_small_image != "":
+    activity.assets.small_image = activity_small_image
+if activity_small_text != "":
+    activity.assets.small_text = activity_small_text
+
+
+
+
+def callback(result):
+    if result == sdk.Result.ok:
+        print("Successfully set the activity!")
+    else:
+        raise Exception(result)
+
+activity_manager.update_activity(activity, callback)
+
+while 1:
+    app.run_callbacks()
+    time.sleep(1)
---
+++ editables.py
@@ -0,0 +1,24 @@
+
+
+
+
+APPLICATION_ID = 1036740499551293602 #edit this to your application, giving you its name and icons
+
+activity_state = "Playing co-op" #edit this to change what the game displays you are doing
+
+#party stuff
+    #edit these to change party max size and current size
+activity_party_size = 1
+activity_party_max_size = 4
+    #dont edit these:
+activity_secrets_join = "my_secret"
+activity_party_id = "my_party_id"
+
+#images
+
+activity_large_image = "tf3_logo" #large image
+activity_large_text = "Titanfall© 3" #large image hover text
+activity_small_image = "devbadge" #small image
+activity_small_text = "Developer Build" #small image hover text
+
+
