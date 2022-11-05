47d54360ca9e7f718e6e45383ced3198d9d38eb4
DINOTICK <79216416+DINOTICK-Dev@users.noreply.github.com>
2022-11-05 11:36:59 -0400

    few bug fixes
    fixed the crashes and invite error for the main branch

--- discord-RPC.py
+++ discord-RPC.py
@@ -1,30 +1,39 @@
 import time
-#importing the variables
+# importing the variables
 from editables import APPLICATION_ID, activity_state, activity_party_size, activity_party_max_size, activity_secrets_join, activity_party_id, activity_large_image, activity_large_text, activity_small_image, activity_small_text
 
-#discord sdk moment
+# discord sdk moment
 import discordsdk as sdk
 
 app = sdk.Discord(APPLICATION_ID, sdk.CreateFlags.default)
 
 activity_manager = app.get_activity_manager()
 
-#set variables for discord rich presence
+# set variables for discord rich presence
 activity = sdk.Activity()
-#set variables for discord rich presence
-#if a variable is blank, it will not be displayed
+# set variables for discord rich presence
+# if a variable is blank, it will not be displayed
 if activity_state != "":
     activity.state = activity_state
 
 
-if activity_party_size != "":
-    activity.party.size.current_size = activity_party_size
-if activity_party_max_size != "":
-    activity.party.size.max_size = activity_party_max_size
+try:
+    if activity_party_size != "":
+        activity.party.size.current_size = activity_party_size
+except:
+    pass
+try:
+    if activity_party_max_size != "":
+        activity.party.size.max_size = activity_party_max_size
+except:
+    pass
+
 if activity_secrets_join != "":
     activity.secrets.join = activity_secrets_join
 if activity_party_id != "":
     activity.party.id = activity_party_id
+else:
+    activity.party.id = "123"
 
 
 if activity_large_image != "":
--- editables.py
+++ editables.py
@@ -1,16 +1,16 @@
 # Where to set your status/activity/rpc information
 
-APPLICATION_ID =  #Integer - edit this to your application, giving you its name and icons
+APPLICATION_ID = 1037103110369321052 #Integer - edit this to your application, giving you its name and icons
 
 activity_state = "ACTIVITY HERE" #String - edit this to change what the game displays you are doing
 
 #party stuff
     # leave these two blank if you don't want to show party size
-activity_party_size = 1 # Integer - edit this to change the party size shown (2 of x)
-activity_party_max_size = 4 # Integer - edit this to change the party size shown (x of 4)
+activity_party_size = "" # Integer - edit this to change the party size shown (2 of x)
+activity_party_max_size = "" # Integer - edit this to change the party size shown (x of 4)
     #dont edit these:
 activity_secrets_join = "party_secret"
-activity_party_id = "party_id"
+activity_party_id = "party id"
 
 #images
 #all of the below need to be strings
